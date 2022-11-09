b07675fc932ee5a855384c96b7e5b27d9a93885e
gocool94 <gocool94@gmail.com>
2022-11-09 16:23:56 +0530

    Ten

---
+++ JAVA_Learning/src/Day_10_While_loop_2/Bank_Account_42.java
@@ -0,0 +1,12 @@
+package Day_10_While_loop_2;
+
+import java.util.Scanner;
+
+public class Bank_Account_42 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
